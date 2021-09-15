03912511551a74360690b8513bba3e271a419a37
carolinahf <herrerac@sas.upenn.edu>
2021-09-15 13:27:03 -0400

    changes
    This is what i did

---
+++ Assignment 1 R Script.R
@@ -0,0 +1,16 @@
+#install.packages("datasets")
+library(datasets)
+
+USArrests
+
+dat<-USArrests
+
+head(dat)
+
+names(dat)
+
+#[1] "Murder"   "Assault"  "UrbanPop" "Rape"    
+
+# This is my answer, when we put it into Rmarkdown we don't need the hashtag. 
+
+
